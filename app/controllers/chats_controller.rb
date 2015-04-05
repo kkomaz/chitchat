a0d515c80166f4ae1e2e91f47bf5394544eadac0
kkomaz <leeajl006@gmail.com>
2015-04-04 22:56:10 -0400

    chat functionality kind of working

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,12 @@
+require "net/http"
 class ChatsController < ApplicationController
   def room
   end
 
   def new_message
+    faye = URI.parse 'http://localhost:9292/faye'
+    message = {:username => session[:username], :msg => params[:message]}.to_json
+    Net::HTTP.post_form(faye, :message => {:channel => '/messages/public', :data => message}.to_json)
+    render :nothing => true
   end
 end

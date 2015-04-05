e990cd112da97f055ae9c2813201b1f52ecdda9a
kkomaz <leeajl006@gmail.com>
2015-04-04 22:36:32 -0400

    add chat room layout

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,7 @@
 class ChatsController < ApplicationController
   def room
   end
+
+  def new_message
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get  '/login' => 'sessions#new', :as => :login
   post '/login' => 'sessions#create'
   get '/chatroom' => 'chats#room', :as => :chat
+  post '/new_message' => 'chats#new_message', :as => :new_message
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
